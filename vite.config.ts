f02b162feaf7442b30f545d7e2c75c754f7737e2
Nelson Herrera <ndanielherrera@icloud.com>
2023-04-25 19:57:17 -0400

    "Updated Vite.Config.ts to have correct name"

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Path-Finding-Visualizer/' : '/',
   plugins: [react()],
 })
